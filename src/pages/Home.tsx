48792b98ec58692329f6251d5e18f478e7d911ff
olaoluwa <olaoluwaayanbola@gmail.com>
2023-11-27 07:39:09 -0800

    menue

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MenuBar from '../components/Menubar'
+const Home = () => {
+  return (
+    <div>
+        <MenuBar>
+          
+        </MenuBar>
+    </div>
+  )
+}
+
+export default Home
